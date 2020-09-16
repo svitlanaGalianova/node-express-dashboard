eef6fbd9b39a7bfc6c5d7e55718c90782eb77b39
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2020-09-15 21:17:17 -0400

    Selecting a log file

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,11 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const dir = process.cwd();
+
 function getDirectoryContents(files, currentDir, query) {
+    var data = []
+    files.forEach(file => {
+        if (isDirectory(currentDir, file)) {
+            data.push({ name: file, isDirectory: true, path: path.join(query, file) })
+        } else {
+            data.push({ name: file, isDirectory: false, path: path.join(query, file), currentDir })
+        }
+    });
+    return data;
 }
 
 function isDirectory(currentDir, file) {
+    const fileInfo = fs.statSync(path.join(currentDir, file))
+    return fileInfo.isDirectory()
 }
 
 function readDir(currentDir, res, query) {
+    fs.readdir(currentDir, (err, files) => {
+        var directoryContents = []
+        if (!err) {
+            directoryContents = getDirectoryContents(files, currentDir, query)
+        }
+        res.json(directoryContents)
+    })
 }
 
 exports.get = (req, res) => {
+    var currentDir = dir;
+    const query = req.query.path ? req.query.path : "";
+    if (query) {
+        currentDir = path.join(currentDir, query)
+    }
+    readDir(currentDir, res, query)
 };
